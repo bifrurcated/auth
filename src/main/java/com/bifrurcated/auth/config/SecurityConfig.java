cfc4d21cdd45be6b27ff110161e969e34d6bc4ec
bifurcated <litvinenko-maks@inbox.ru>
2022-12-01 19:19:52 +0900

    Add cors for security config

--- src/main/java/com/bifrurcated/auth/config/SecurityConfig.java
+++ src/main/java/com/bifrurcated/auth/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,6 +13,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/**")
--- src/main/java/com/bifrurcated/auth/config/WebMvcConfig.java
+++ src/main/java/com/bifrurcated/auth/config/WebMvcConfig.java
@@ -8,8 +8,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
-
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     private final AuthorizationInterceptor authorizationInterceptor;
