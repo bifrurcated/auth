748a1bd9e815604a8a66605c54b293661e3c36df
bifurcated <litvinenko-maks@inbox.ru>
2022-12-01 22:41:49 +0900

    Change to other error code and message

--- src/main/java/com/bifrurcated/auth/interceptor/AuthorizationInterceptor.java
+++ src/main/java/com/bifrurcated/auth/interceptor/AuthorizationInterceptor.java
@@ -1,8 +1,10 @@
 package com.bifrurcated.auth.interceptor;
 
 import com.bifrurcated.auth.error.NoBearerTokenError;
+import com.bifrurcated.auth.error.UnauthenticatedError;
 import com.bifrurcated.auth.service.AuthService;
 import org.springframework.http.HttpHeaders;
+import org.springframework.lang.NonNullApi;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -18,10 +20,13 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
-        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+        if (authorizationHeader == null) {
+            throw new UnauthenticatedError();
+        }
+        if (!authorizationHeader.startsWith("Bearer ")) {
             throw new NoBearerTokenError();
         }
 
